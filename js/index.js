fae3064946951a178ef000b694125197a45f3584
imko <kujijieep@gmail.com>
2020-12-22 06:19:14 +0300

    doctors-detail

--- js/index.js
+++ js/index.js
@@ -3,3 +3,12 @@ document.querySelector("form button").onclick = () => {
 	document.querySelector("#signup h2").innerHTML = "Спасибо";
 	document.querySelector("#signup .subtitle").innerHTML = "В ближайшее время с Вами свяжется администратор. Напоминаем, что мы работаем без выходных с 9:00 до 21:00.";
 };
+
+document.querySelectorAll(".subnav a").forEach((a) => {
+	a.onclick = () => {
+		document.querySelectorAll(".subnav a").forEach((aa) => {
+			aa.classList.remove("active");
+		});
+		a.classList.add("active");
+	};
+});
