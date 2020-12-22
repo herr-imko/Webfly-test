7a1e25c2f60e7220c00751d75c9a432d9fa0aef6
imko <kujijieep@gmail.com>
2020-12-22 13:21:16 +0300

    финал

--- js/index.js
+++ js/index.js
@@ -4,24 +4,26 @@ document.querySelector("form button").onclick = () => {
 	document.querySelector("#signup .subtitle").innerHTML = "В ближайшее время с Вами свяжется администратор. Напоминаем, что мы работаем без выходных с 9:00 до 21:00.";
 };
 
-document.querySelectorAll(".subnav a").forEach((a) => {
-	a.onclick = () => {
-		document.querySelectorAll(".subnav a").forEach((aa) => {
-			aa.classList.remove("active");
-		});
-		a.classList.add("active");
-	};
-});
+if (window.screen.width > 1024) {
+	document.querySelectorAll(".subnav a").forEach((a) => {
+		a.onclick = () => {
+			document.querySelectorAll(".subnav a").forEach((aa) => {
+				aa.classList.remove("active");
+			});
+			a.classList.add("active");
+		};
+	});
+} else {
+	$("footer .title").on("click", function () {
+		$(this).toggleClass("active");
+		$(this).next().slideToggle();
+	});
 
-$("footer .title").on("click", function () {
-	$(this).toggleClass("active");
-	$(this).next().slideToggle();
-});
+	$("header .top .more").on("click", function () {
+		$("header .bot").toggleClass("show");
+	});
 
-$("header .top .more").on("click", function () {
-	$("header .bot").toggleClass('show');
-});
-
-$("header .bot .close").on("click", function () {
-	$("header .bot").toggleClass('show')
-});
+	$("header .bot .close").on("click", function () {
+		$("header .bot").toggleClass("show");
+	});
+}
