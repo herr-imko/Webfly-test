1a701b26b6ed0cacdb37e98b3bceba7a0586674e
Никита Вариводин <kujijiep@gmail.com>
2021-01-13 17:11:02 +0300

    правки

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.querySelector("form ").onsubmit = () => {
 	document.querySelector("#signup .subtitle").innerHTML = "В ближайшее время с Вами свяжется администратор. Напоминаем, что мы работаем без выходных с 9:00 до 21:00.";
 };
 
-if (window.screen.width > 1024) {
+if (window.innerWidth > 1024) {
 	document.querySelectorAll(".subnav a").forEach((a) => {
 		a.onclick = () => {
 			document.querySelectorAll(".subnav a").forEach((aa) => {
