7694464f661512deffa39279c1d28ae391291bf9
herr-imko <58493296+herr-imko@users.noreply.github.com>
2021-02-17 11:10:48 +0300

    /

--- js/index.js
+++ js/index.js
@@ -1,77 +1,79 @@
-document.querySelector("form ").onsubmit = () => {
-	event.preventDefault();
-	document.querySelector("form").remove();
-	document.querySelector("#signup h2").innerHTML = "Спасибо";
-	document.querySelector("#signup .subtitle").innerHTML = "В ближайшее время с Вами свяжется администратор. Напоминаем, что мы работаем без выходных с 9:00 до 21:00.";
-};
+$(document).ready(function () {
+	document.querySelector("form ").onsubmit = () => {
+		event.preventDefault();
+		document.querySelector("form").remove();
+		document.querySelector("#signup h2").innerHTML = "Спасибо";
+		document.querySelector("#signup .subtitle").innerHTML = "В ближайшее время с Вами свяжется администратор. Напоминаем, что мы работаем без выходных с 9:00 до 21:00.";
+	};
 
-if (window.innerWidth > 1024) {
-	document.querySelectorAll(".subnav a").forEach((a) => {
-		a.onclick = () => {
-			document.querySelectorAll(".subnav a").forEach((aa) => {
-				aa.classList.remove("active");
-			});
-			a.classList.add("active");
-		};
-	});
-} else {
-	$("footer .title").on("click", function () {
-		$(this).toggleClass("active");
-		$(this).next().slideToggle();
-	});
-
-	$("header .top .more").on("click", function () {
-		$("header .bot").toggleClass("show");
-	});
-
-	$("header .bot .close").on("click", function () {
-		$("header .bot").toggleClass("show");
-	});
-
-	$("header .bot .button-primary.d-mobile").on("click", function () {
-		$("header .bot").toggleClass("show");
-	});
-}
-
-$("#signup .input-wrapper input").on("input", function () {
-	this.value = this.value.replace(/[^0-9]/g, "");
-});
-
-if (document.querySelector(".subnav")) {
-	window.addEventListener("scroll", function () {
-		let sects = {};
-
-		$(".subnav a").each(function () {
-			sects[$(this).attr("href")] = null;
+	if (window.innerWidth > 1024) {
+		document.querySelectorAll(".subnav a").forEach((a) => {
+			a.onclick = () => {
+				document.querySelectorAll(".subnav a").forEach((aa) => {
+					aa.classList.remove("active");
+				});
+				a.classList.add("active");
+			};
+		});
+	} else {
+		$("footer .title").on("click", function () {
+			$(this).toggleClass("active");
+			$(this).next().slideToggle();
 		});
 
-		for (const key in sects) {
-			sects[key] = $(key).offset().top;
-		}
+		$("header .top .more").on("click", function () {
+			$("header .bot").toggleClass("show");
+		});
 
-		for (const key in sects) {
-			if (window.scrollY + 100 >= sects[key]) {
-				var lastScroll = key.slice(1);
-			}
-		}
+		$("header .bot .close").on("click", function () {
+			$("header .bot").toggleClass("show");
+		});
 
-		if (lastScroll) {
-			document.querySelectorAll(".subnav a").forEach((a) => {
-				a.classList.remove("active");
+		$("header .bot .button-primary.d-mobile").on("click", function () {
+			$("header .bot").toggleClass("show");
+		});
+	}
+
+	$("#signup .input-wrapper input").on("input", function () {
+		this.value = this.value.replace(/[^0-9]/g, "");
+	});
+
+	if (document.querySelector(".subnav")) {
+		window.addEventListener("scroll", function () {
+			let sects = {};
+
+			$(".subnav a").each(function () {
+				sects[$(this).attr("href")] = null;
 			});
-			$(`a[href="#${lastScroll}"]`).addClass("active");
-		}
-	});
 
-	$(".subnav a").on("click", function () {
-		event.preventDefault();
-		console.log(window.scrollY, $($(this).attr("href")).offset().top);
-		$([document.documentElement, document.body]).animate(
-			{
-				scrollTop: $($(this).attr("href")).offset().top,
-			},
-			200,
-			"linear"
-		);
-	});
-}
+			for (const key in sects) {
+				sects[key] = $(key).offset().top;
+			}
+
+			for (const key in sects) {
+				if (window.scrollY + 100 >= sects[key]) {
+					var lastScroll = key.slice(1);
+				}
+			}
+
+			if (lastScroll) {
+				document.querySelectorAll(".subnav a").forEach((a) => {
+					a.classList.remove("active");
+				});
+				$(`a[href="#${lastScroll}"]`).addClass("active");
+			}
+		});
+
+		$(".subnav a").on("click", function () {
+			event.preventDefault();
+			console.log(window.scrollY, $($(this).attr("href")).offset().top);
+			$([document.documentElement, document.body]).animate(
+				{
+					scrollTop: $($(this).attr("href")).offset().top,
+				},
+				200,
+				"linear"
+			);
+		});
+	}
+});
